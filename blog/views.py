ea859238c1dfa805e73930150f18e77f8beb4b8a
Jon Thorpe <jonthrp@yahoo.com>
2017-03-01 15:39:33 -0800

    Includes all extension exercises except limiting number of entries per page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from flask import render_template
+from flask import flash
+
+from flask.ext.login import login_user
+from flask.ext.login import login_required
+
+from werkzeug.security import check_password_hash
+from .database import User
 
 from . import app
 from .database import session, Entry
@@ -33,12 +40,14 @@ def entries(page=1):
     )
 
 @app.route("/entry/add", methods=["GET"])
+@login_required
 def add_entry_get():
     return render_template("add_entry.html")
 
 from flask import request, redirect, url_for
 
 @app.route("/entry/add", methods=["POST"])
+@login_required
 def add_entry_post():
     entry = Entry(
         title=request.form["title"],
@@ -48,20 +57,61 @@ def add_entry_post():
     session.commit()
     return redirect(url_for("entries"))
 
-@app.route("/entry/<id>/delete", methods=["DELETE"])
-def delete_entry(id):
-    session.execute('DELETE from entries WHERE id = id', [id])
-    session.commit()
-    return redirect(url_for("entries"))
-
 
 @app.route("/entry/<id>")
 def single_entry(id):
     entry = session.query(Entry)
-    entry = entry.filter_by(id=id)
-    return render_template("single_entry.html",
-                           entries=entry,
-                           id=id
-                           )
+    entry = entry.filter(Entry.id==id)
+    entry = entry.one()
+    return render_template("single_entry.html", entry=entry)
 
 
+@app.route("/entry/<id>/edit", methods=["GET"])
+@login_required
+def edit_entry_get(id):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id==id)
+    entry = entry.one()
+    return render_template("edit_entry.html", entry=entry)
+
+
+@app.route("/entry/<id>/edit", methods=["POST"])
+@login_required
+def edit_entry_post(id):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id==id)
+    entry = entry.one()
+    session.delete(entry)
+    entry = Entry(title=request.form["title"], content=request.form["content"])
+    session.add(entry)
+    session.commit()
+    return redirect(url_for("entries"))
+
+
+@app.route("/entry/<id>/delete")
+@login_required
+def delete_entry(id):
+    entry = session.query(Entry)
+    entry = entry.filter(Entry.id==id)
+    entry = entry.one()
+    session.delete(entry)
+    session.commit()
+    return redirect(url_for("entries"))
+
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
